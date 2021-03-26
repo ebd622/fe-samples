ee3a8a18913926603ce5ad428ed726222e8eaa4a
Eugeny Burdo <eburdo@gmail.com>
2021-03-26 22:41:58 +0100

    initial commit

---
+++ ang-app/src/app/app.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppComponent } from './app.component';
+import {ServerComponent} from './server/server.component';
+import {FormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
+import { ServersComponent } from './servers/servers.component';
+
+// @NgModule is a decorator
+@NgModule({
+  declarations: [
+    AppComponent,
+    ServerComponent,
+    ServersComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
