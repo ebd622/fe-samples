b262d1637c24523e0e141c9ead4f297faccd9704
Eugeny Burdo <eburdo@gmail.com>
2021-04-04 01:03:47 +0200

    L54: Output recipe list

--- coock-book/src/app/recipes/recipes-list/recipes-list.component.ts
+++ coock-book/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -8,6 +8,7 @@ import {Recipe} from '../recipe.model';
 })
 export class RecipesListComponent implements OnInit {
   recepies: Recipe[]  = [
+    new Recipe('A test name', 'This is a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'),
     new Recipe('A test name', 'This is a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg')
   ];
 
