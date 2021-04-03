f39717054b3516d31933f00d8ccb6031e64f06df
Eugeny Burdo <eburdo@gmail.com>
2021-04-04 00:53:52 +0200

    L53: add content to Recipe

--- coock-book/src/app/recipes/recipes-list/recipes-list.component.ts
+++ coock-book/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Recipe} from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recepies: Recipe[]  = [
+    new Recipe('A test name', 'This is a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg')
+  ];
 
   constructor() { }
 
