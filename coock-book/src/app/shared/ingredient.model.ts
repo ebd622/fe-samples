9832d9df8c92fe1dd5d0fad2742c30f3e739b7e5
Eugeny Burdo <eburdo@gmail.com>
2021-04-04 14:16:21 +0200

    L57: add shared class

---
+++ coock-book/src/app/shared/ingredient.model.ts
@@ -0,0 +1,10 @@
+/*
+  This is a shortcut:
+  - "name" and "amount" will be automatically created in the class
+  - They will get values provided in the construotor
+*/
+
+export class Ingredient {
+  constructor(public name: string, public amount: number){
+  }
+}
