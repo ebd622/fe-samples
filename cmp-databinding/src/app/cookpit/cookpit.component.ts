69021ea903ffc5325e21b0ab4b3a8f0403854c34
Eugeny Burdo <eburdo@gmail.com>
2021-04-05 15:53:58 +0200

    L69: Binding to custom events

--- cmp-databinding/src/app/cookpit/cookpit.component.ts
+++ cmp-databinding/src/app/cookpit/cookpit.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 
 @Component({
   selector: 'app-cookpit',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./cookpit.component.css']
 })
 export class CookpitComponent implements OnInit {
+  /*Event properties*/
+  @Output() serverCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+  @Output() blueprintCreated = new EventEmitter<{serverName: string, serverContent: string}>();
+
   newServerName = '';
   newServerContent = '';
 
@@ -14,19 +18,17 @@ export class CookpitComponent implements OnInit {
   ngOnInit(): void {
   }
   onAddServer() {
-    // this.serverElements.push({
-    //   type: 'server',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // });
+    this.serverCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });
   }
 
   onAddBlueprint() {
-    // this.serverElements.push({
-    //   type: 'blueprint',
-    //   name: this.newServerName,
-    //   content: this.newServerContent
-    // });
+    this.blueprintCreated.emit({
+      serverName: this.newServerName,
+      serverContent: this.newServerContent
+    });
   }
 
 }
