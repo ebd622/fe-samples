6b2b1d1d9c6a9a1058f2d37c5dc9f7fef529ed37
Eugeny Burdo <eburdo@gmail.com>
2021-04-05 15:10:47 +0200

    L68: use an alias for a property

--- cmp-databinding/src/app/server-element/server-element.component.ts
+++ cmp-databinding/src/app/server-element/server-element.component.ts
@@ -7,7 +7,11 @@ import {Component, Input, OnInit} from '@angular/core';
 })
 export class ServerElementComponent implements OnInit {
   /*Use "Input"-decorator to expose the property "element" to a word and make it available to other components */
+  /* Option 1:
   @Input() element: {type: string, name: string, content: string};
+  */
+  /*Option 2: use alias 'serElelemnt'. The alias should be used in other components*/
+  @Input('srvElement') element: {type: string, name: string, content: string};
 
   constructor() { }
 
