185a4c835b332f52168ebbc2e639a314b1656b80
Eugeny Burdo <eburdo@gmail.com>
2021-04-05 19:22:40 +0200

    L78: use lifecycle hooks

--- cmp-databinding/src/app/server-element/server-element.component.ts
+++ cmp-databinding/src/app/server-element/server-element.component.ts
@@ -1,11 +1,11 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 
 @Component({
   selector: 'app-server-element',
   templateUrl: './server-element.component.html',
   styleUrls: ['./server-element.component.css']
 })
-export class ServerElementComponent implements OnInit {
+export class ServerElementComponent implements OnInit, OnChanges {
   /*Use "Input"-decorator to expose the property "element" to a word and make it available to other components */
   /* Option 1:
   @Input() element: {type: string, name: string, content: string};
@@ -13,9 +13,16 @@ export class ServerElementComponent implements OnInit {
   /*Option 2: use alias 'serElelemnt'. The alias should be used in other components*/
   @Input('srvElement') element: {type: string, name: string, content: string};
 
-  constructor() { }
+  constructor() {
+    console.log('constructor called!');
+  }
+  ngOnChanges(changes: SimpleChanges): void {
+    console.log('ngOnChanges called!');
+    console.log(changes);
+  }
 
   ngOnInit(): void {
+    console.log('ngOnInit called!');
   }
 
 }
