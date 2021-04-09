f3f26fec1af76e769183fe3fe40a4fe4569b5d5e
Eugeny Burdo <eburdo@gmail.com>
2021-04-09 23:03:13 +0200

    L88: Event and Property bindning

--- coock-book/src/app/recipes/recipes-list/recipes-list.component.ts
+++ coock-book/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
 
 @Component({
@@ -7,9 +7,10 @@ import {Recipe} from '../recipe.model';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  @Output() recipeWasSelected = new  EventEmitter<Recipe>();
   recepies: Recipe[]  = [
-    new Recipe('A test name', 'This is a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'),
-    new Recipe('A test name', 'This is a test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg')
+    new Recipe('A test name 1', 'This is a test 1 ', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'),
+    new Recipe('A test name 2' , 'This is a test 2', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg')
   ];
 
   constructor() { }
@@ -17,4 +18,8 @@ export class RecipesListComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  onRecipeSelected(rcp: Recipe){
+    this.recipeWasSelected.emit(rcp);
+
+  }
 }
