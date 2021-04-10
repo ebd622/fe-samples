5dfe1baacd98b28ae9ce99dad62d74aed6f28344
Eugeny Burdo <eburdo@gmail.com>
2021-04-10 11:18:06 +0200

    L90: use local references

--- coock-book/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ coock-book/src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, ElementRef, EventEmitter, OnInit, Output, ViewChild} from '@angular/core';
+import {Ingredient} from '../../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-edit',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-edit.component.css']
 })
 export class ShoppingEditComponent implements OnInit {
+  /* nameInput and amountInput are local references */
+  @ViewChild('nameInput', {static: false})nameInputRef: ElementRef;
+  @ViewChild('amountInput', {static: false})amountInputRef: ElementRef;
+  @Output() ingredientAdded = new EventEmitter<Ingredient>();
 
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  onAddButton(){
+    const name = this.nameInputRef.nativeElement.value;
+    const amount = this.amountInputRef.nativeElement.value;
+    const newIngredient = new Ingredient(name, amount);
+    this.ingredientAdded.emit(newIngredient);
+
+  }
 }
