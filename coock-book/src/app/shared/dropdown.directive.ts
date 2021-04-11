8f4ee5d4e5156d9ca99130bd41ee91deb3e99631
Eugeny Burdo <eburdo@gmail.com>
2021-04-11 11:59:05 +0200

    L104: Closing the Dropdown From Anywhere

--- coock-book/src/app/shared/dropdown.directive.ts
+++ coock-book/src/app/shared/dropdown.directive.ts
@@ -1,11 +1,20 @@
-import {Directive, HostBinding, HostListener} from '@angular/core';
+import {Directive, ElementRef, HostBinding, HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appDropdown]'
 })
 export class DropdownDirective {
   @HostBinding('class.open') isOpen = false;
-  @HostListener('click') toggleOpen(){
-    this.isOpen = !this.isOpen;
+
+  // Option1: Close menu when clicking a menu-button
+  // @HostListener('click') toggleOpen(){
+  //   this.isOpen = !this.isOpen;
+  // }
+
+  /*Option1: Close menu when clicking anywhere outside*/
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
   }
+  /* "private elRef: ElementRef" - will also create a class variable*/
+  constructor(private elRef: ElementRef) {}
 }
