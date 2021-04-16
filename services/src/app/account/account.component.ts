7c55a405149a2c90bd25b695d22b0a961a9b2034
Eugeny Burdo <eburdo@gmail.com>
2021-04-16 22:16:24 +0200

    L108: Dependency injection

--- services/src/app/account/account.component.ts
+++ services/src/app/account/account.component.ts
@@ -1,18 +1,21 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
+import {LoggingService} from '../logging.service';
 
 @Component({
   selector: 'app-account',
   templateUrl: './account.component.html',
-  styleUrls: ['./account.component.css']
+  styleUrls: ['./account.component.css'],
+  providers: [LoggingService] /*Inform Angular how to crete a service*/
 })
 export class AccountComponent {
   @Input() account: {name: string, status: string};
   @Input() id: number;
   @Output() statusChanged = new EventEmitter<{id: number, newStatus: string}>();
 
+  constructor(private logginService: LoggingService){} /*Create a service "logginService": dependency injection */
 
   onSetTo(status: string) {
     this.statusChanged.emit({id: this.id, newStatus: status});
-    console.log('A server status changed, new status: ' + status);
+    this.logginService.logStatusChange(status);
   }
 }
