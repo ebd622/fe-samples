5ef6cea5a7e650a2f606e8729040b6192f302eba
Eugeny Burdo <eburdo@gmail.com>
2021-04-18 13:46:20 +0200

    L122: add ingredients to Recipes

--- coock-book/src/app/recipes/recipe.service.ts
+++ coock-book/src/app/recipes/recipe.service.ts
@@ -1,11 +1,26 @@
 import {Recipe} from './recipe.model';
 import {EventEmitter} from '@angular/core';
+import {Ingredient} from '../shared/ingredient.model';
 
 export class RecipeService{
   recipeSelected = new EventEmitter<Recipe>();
   private recepies: Recipe[]  = [
-    new Recipe('A test name 1', 'This is a test 1 ', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'),
-    new Recipe('A test name 2' , 'This is a test 2', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg')
+    new Recipe(
+      'A test name 1',
+      'This is a test 1 ',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg',
+      [
+        new Ingredient('Meat', 1),
+        new Ingredient('French Fries', 20)
+      ]),
+    new Recipe(
+      'A test name 2' ,
+      'This is a test 2',
+      'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg',
+      [
+        new Ingredient('Buns', 2),
+        new Ingredient('French Fries1', 20)
+      ])
   ];
 
   getRecipes(){
