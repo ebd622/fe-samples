fa2a2250150838809097c75c1f9e47fe96e491aa
Eugeny Burdo <eburdo@gmail.com>
2021-04-18 12:22:45 +0200

    L118: Managinf Recipes

--- coock-book/src/app/recipes/recipe.service.ts
+++ coock-book/src/app/recipes/recipe.service.ts
@@ -1,3 +1,12 @@
-export class RecipeService{
+import {Recipe} from './recipe.model';
 
+export class RecipeService{
+  private recepies: Recipe[]  = [
+    new Recipe('A test name 1', 'This is a test 1 ', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'),
+    new Recipe('A test name 2' , 'This is a test 2', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg')
+  ];
+
+  getRecipes(){
+    return this.recepies.slice(); //this will return not a refference to "recipes" but a copy of the array
+  }
 }
--- coock-book/src/app/recipes/recipes-list/recipes-list.component.ts
+++ coock-book/src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,5 +1,6 @@
 import {Component, EventEmitter, OnInit, Output} from '@angular/core';
 import {Recipe} from '../recipe.model';
+import {RecipeService} from '../recipe.service';
 
 @Component({
   selector: 'app-recipes-list',
@@ -8,14 +9,12 @@ import {Recipe} from '../recipe.model';
 })
 export class RecipesListComponent implements OnInit {
   @Output() recipeWasSelected = new  EventEmitter<Recipe>();
-  recepies: Recipe[]  = [
-    new Recipe('A test name 1', 'This is a test 1 ', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg'),
-    new Recipe('A test name 2' , 'This is a test 2', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg')
-  ];
+  recipes: Recipe[];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit(): void {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(rcp: Recipe){
