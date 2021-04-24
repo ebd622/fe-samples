d96e3c5d32eede22bfbb1b16ba58ab29d1e4b6ac
Eugeny Burdo <eburdo@gmail.com>
2021-04-24 12:26:02 +0200

    L136: add unsubscribe

--- routing/src/app/users/user/user.component.ts
+++ routing/src/app/users/user/user.component.ts
@@ -1,14 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
 import {__param} from 'tslib';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy{
   user: {id: number, name: string};
+  paramSubscription: Subscription;
 
   constructor(private route: ActivatedRoute) { }
 
@@ -18,13 +20,17 @@ export class UserComponent implements OnInit {
       name: this.route.snapshot.params['name']
     };
     // Create obsevable: user will be changes when parameters (id, name) are changed
-    this.route.params
+    this.paramSubscription = this.route.params
       .subscribe(
         (params: Params) => {
           this.user.id = params['id'];
           this.user.name = params['name'];
         }
       );
+  }
+  ngOnDestroy(): void {
+    // unsubscribe from the observabale when a component is destroyed
+    this.paramSubscription.unsubscribe();
   }
 
 }
