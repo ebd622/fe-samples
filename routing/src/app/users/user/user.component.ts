e2f2b28fa92acf6e02a2be50886291dd76fade84
Eugeny Burdo <eburdo@gmail.com>
2021-04-24 12:15:37 +0200

    L135: Fetching route patams reactively

--- routing/src/app/users/user/user.component.ts
+++ routing/src/app/users/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
+import {ActivatedRoute, Params} from '@angular/router';
+import {__param} from 'tslib';
 
 @Component({
   selector: 'app-user',
@@ -16,6 +17,14 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
+    // Create obsevable: user will be changes when parameters (id, name) are changed
+    this.route.params
+      .subscribe(
+        (params: Params) => {
+          this.user.id = params['id'];
+          this.user.name = params['name'];
+        }
+      );
   }
 
 }
