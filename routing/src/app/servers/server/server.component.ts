067e2b91d4c0f71d2b0cf518bd5f0aa1c36fdf08
Eugeny Burdo <eburdo@gmail.com>
2021-04-27 13:40:39 +0200

    L152: resolve dynamic data

--- routing/src/app/servers/server/server.component.ts
+++ routing/src/app/servers/server/server.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 import { ServersService } from '../servers.service';
-import {ActivatedRoute, Params, Router} from '@angular/router';
+import {ActivatedRoute, Data, Params, Router} from '@angular/router';
 
 @Component({
   selector: 'app-server',
@@ -16,15 +16,22 @@ export class ServerComponent implements OnInit {
               private router: Router) { }
 
   ngOnInit() {
-    const id = +this.route.snapshot.params['id'];
-    this.server = this.serversService.getServer(id);
-    this.route.params
-      .subscribe(
-        (params: Params) => {
-          this.server = this.serversService.getServer(+params['id']);
-        }
-      );
+    // Option 1:
+    // const id = +this.route.snapshot.params['id'];
+    // this.server = this.serversService.getServer(id);
+    // this.route.params
+    //   .subscribe(
+    //     (params: Params) => {
+    //       this.server = this.serversService.getServer(+params['id']);
+    //     }
+    //   );
 
+    // Option 2: use resolver
+    this.route.data.subscribe(
+      (data: Data) => {
+        this.server = data['server'];
+      }
+    )
   }
 
   onEdit(){
