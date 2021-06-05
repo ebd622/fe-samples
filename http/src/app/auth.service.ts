66ae221f9478e7e1624ca922e23edcd4cde9cba2
Eugeny Burdo <eburdo@gmail.com>
2021-06-05 10:37:53 +0200

    L268: add query patams

--- http/src/app/auth.service.ts
+++ http/src/app/auth.service.ts
@@ -1,15 +1,40 @@
-import {HttpHeaders} from '@angular/common/http';
+import {HttpHeaders, HttpParams} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 
 @Injectable({providedIn: 'root'})
 export class AuthService {
-  private httpOptions = {
-    headers: new HttpHeaders({
-      'Content-Type':  'application/json',
-      'Authorization': 'Basic ' + btoa('admin:secret')
-    })
-  };
+  private httpAuth = new HttpHeaders({
+    'Content-Type':  'application/json',
+    'Authorization': 'Basic ' + btoa('admin:secret')
+  }) ;
+
+  // ************** Just another option ****
+  // private httpOptions = {
+  //   headers: new HttpHeaders({
+  //     'Content-Type':  'application/json',
+  //     'Authorization': 'Basic ' + btoa('admin:secret')
+  //   })
+  // };
+  // getAuthHeaders() {
+  //   return this.httpOptions;
+  // }
+  // ***********************************
+
   getAuthHeaders() {
-    return this.httpOptions;
+    return {
+      headers: this.httpAuth
+    }
   }
+
+  // This is just an example on how to create params
+  getAuthHeadersAndParams() {
+    let searchParams = new HttpParams();
+    searchParams = searchParams.append('key1', 'value1');
+    searchParams = searchParams.append('key2', 'value2');
+    return {
+      headers: this.httpAuth,
+      params: searchParams
+    }
+  }
+
 }
