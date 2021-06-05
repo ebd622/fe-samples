3bafab5c8cb14d338138c029f931294a82d97174
Eugeny Burdo <eburdo@gmail.com>
2021-06-05 12:02:40 +0200

    L272L: Use interceptors for auth

--- http/src/app/auth-interceptor.service.ts
+++ http/src/app/auth-interceptor.service.ts
@@ -1,4 +1,4 @@
-import {HttpHandler, HttpInterceptor, HttpRequest} from '@angular/common/http';
+import {HttpHandler, HttpHeaders, HttpInterceptor, HttpRequest} from '@angular/common/http';
 
 /*
 Here we intercept all requests before they will leave the application
@@ -7,6 +7,18 @@ See also app.modules.ts
 export class AuthInterceptorService implements HttpInterceptor{
   intercept(req: HttpRequest<any>, next: HttpHandler){
     console.log('Request on its way')
+
+    // ******************************************************************
+    // Here we can modify an original request. We can not change it, because it is imputable. We need to clone it.
+    // This is a better way how to implement Authentication!!!
+    // const modifiedRequest = req.clone({
+    //   headers: req.headers
+    //      .append('Content-Type',  'application/json')
+    //      .append('Authorization', 'Basic ' + btoa('admin:secret'))
+    // });
+    // return next.handle(modifiedRequest);
+    // ******************************************************************
+
     return next.handle(req);
   }
 }
