dcbdea00521ec0ba4323b6e4ba1fc7b3b300c9b1
Eugeny Burdo <eburdo@gmail.com>
2021-06-06 11:46:07 +0200

    L288: switch between modes

--- auth/src/app/auth/auth.component.ts
+++ auth/src/app/auth/auth.component.ts
@@ -4,4 +4,11 @@ import { Component } from '@angular/core';
   selector: 'app-auth',
   templateUrl: './auth.component.html'
 })
-export class AuthComponent {}
+export class AuthComponent {
+  isLoginMode = true;
+
+  onSwitchMode(){
+    // reverse value
+    this.isLoginMode = !this.isLoginMode;
+  }
+}
