549ef9d58cdae4475739c230290c9340c6b9320f
Eugeny Burdo <eburdo@gmail.com>
2021-06-10 14:07:34 +0200

    L325: create shopping-list feature module

---
+++ modules-optimizing/src/app/shopping-list/shopping-list.module.ts
@@ -0,0 +1,21 @@
+import {NgModule} from '@angular/core';
+import {ShoppingListComponent} from './shopping-list.component';
+import {ShoppingEditComponent} from './shopping-edit/shopping-edit.component';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {CommonModule} from '@angular/common';
+import {RouterModule} from '@angular/router';
+
+@NgModule({
+  declarations: [
+    ShoppingListComponent,
+    ShoppingEditComponent
+  ],
+  imports: [
+    CommonModule,
+    FormsModule,
+    RouterModule.forChild([
+      { path: 'shopping-list', component: ShoppingListComponent }
+    ])
+  ]
+})
+export class ShoppingListModule{}
