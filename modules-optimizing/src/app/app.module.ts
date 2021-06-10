0e4103eb710d3e6aa6249149f5784ebbadc5ff34
Eugeny Burdo <eburdo@gmail.com>
2021-06-10 17:19:15 +0200

    L328: adding an Auth Feature Module

--- modules-optimizing/src/app/app.module.ts
+++ modules-optimizing/src/app/app.module.ts
@@ -1,33 +1,32 @@
 import {BrowserModule} from '@angular/platform-browser';
 import {NgModule} from '@angular/core';
-import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {FormsModule} from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
 
 import {AppComponent} from './app.component';
 import {HeaderComponent} from './header/header.component';
 import {AppRoutingModule} from './app-routing.module';
-import {AuthComponent} from './auth/auth.component';
 import {RecipesModule} from './recipes/recipes.module';
 import {ShoppingListModule} from './shopping-list/shopping-list.module';
 import {SharedModule} from './shared/shared.module';
 import {CoreModules} from './core.modules';
+import {AuthModul} from './auth/auth.modul';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    AuthComponent
   ],
   imports: [
     BrowserModule,
     FormsModule,
-    ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
     RecipesModule,
     ShoppingListModule,
     SharedModule,
-    CoreModules
+    CoreModules,
+    AuthModul
   ],
   bootstrap: [AppComponent]
 })
